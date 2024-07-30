43f186d2bb37f805fc3c4df7c54f4146feaf6ccc
22lll <1348658724@qq.com>
2024-07-30 10:33:24 +0800

    添加测试用例

---
+++ main.js
@@ -0,0 +1,26 @@
+import App from './App'
+
+// #ifndef VUE3
+import Vue from 'vue'
+Vue.config.productionTip = false
+
+// 全局注册
+import zjw_GoogleMap from './components/zjw-GoogleMap'
+Vue.component('zjw-GoogleMap', zjw_GoogleMap)
+
+App.mpType = 'app'
+const app = new Vue({
+    ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
---
+++ pages/test/test.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+			// #ifdef APP-PLUS
+			// this.playVideo()
+			uni.$on('drawer-page', (data) => {
+				uni.showToast({
+					title: '点击了第' + data + '项',
+					icon: "none"
+				});
+			})
+			// #endif
+		},
+		onUnload() {
+			uni.$off('drawer-page')
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
